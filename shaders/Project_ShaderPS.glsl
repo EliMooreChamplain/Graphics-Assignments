8792d399c3200d6d444d62472c91021efc04d9a6
EliMooreChamplain <eli.moore@mymail.champlain.edu>
2020-11-05 20:23:54 -0500

    Progress made

--- shaders/Project_ShaderPS.glsl
+++ shaders/Project_ShaderPS.glsl
@@ -1,14 +1,5 @@
 #version 330
 
-in vec2 vTexCoord;
-in vec3 normal;
-in vec4 position;
-in float time;
-
-uniform sampler2D uTex;
-
-layout (location = 0) out vec4 rtFragColor;
-
 struct sPL
 {
 	vec4 center;
@@ -16,51 +7,87 @@ struct sPL
 	float intensity;
 };
 
-void main() {
-	
-	sPL pl;
-	pl.center = vec4(5,5,5,1);
-	pl.color = vec4(1,1,1,1);
-	pl.intensity = 1000.0;
-	
+in vec2 vTexCoord;
+in vec3 vNormal;
+in vec4 vPosition;
+in float vTime;
+in float vVertexDiffuse;
+in sPL pl;
+
+uniform sampler2D uTex;
+uniform sampler2D uSpecularMask;
+uniform sampler2D uLightMask;
+
+layout (location = 0) out vec4 rtFragColor;
+
+
+
+float fragDiffuseIntensity()
+{
 	vec3 lightVector = normalize(pl.center.xyz);
-	float diffuseCoefficient = max(0.0,dot(normal,lightVector));
+	float diffuseCoefficient = max(0.0,dot(vNormal,lightVector));
 	
-	float slDist = distance(pl.center.xyz,position.xyz);
+	float slDist = distance(pl.center.xyz,vPosition.xyz);
 	
 	//Distance over intensity
     float dOi = (slDist/pl.intensity);
     
     float attenuatedIntensity = 1.0/(1.0 + dOi + (dOi * dOi));
     
-    float diffuseIntensity = diffuseCoefficient * attenuatedIntensity;
+    return diffuseCoefficient * attenuatedIntensity;
+}
+
+float calcSpecIntensity(float specular)
+{
+	vec3 lightVector = normalize(pl.center.xyz);
+	
     
     float specIntensity;   //Specular Intensity
     float specCo;          //Specular Coefficient
     vec3 viewVec;          //View Vector
     vec3 halfVec;          //Halfway Vector
     
-    viewVec = normal;
+    viewVec = vNormal;
         
     halfVec = normalize(lightVector + viewVec);
         
-    specCo = max(0.0,dot(normal,halfVec));
+    specCo = max(0.0,dot(vNormal,halfVec));
     
-    specIntensity = pow(specCo, 16.0 * 4.0);
+    return pow(specCo, specular * 4.0);
+}
+
+void main() {
 	
-	vec2 offset = vec2(time * 0.125,0);
+	vec2 offset = vec2(0.0,0.0);//vec2(time * 0.125,0);
+	
+	float specular = 16.0;
+	float specularMask = 1.0 - texture(uSpecularMask, vTexCoord + offset).x;
+	
+	float nightLightIntensity = 0.75;
+	vec4 nightLightColor = vec4(0.949, 0.756, 0.109, 1.0);
+	vec4 lightMask = texture(uLightMask, vTexCoord + offset).x * nightLightColor * nightLightIntensity;
+	
+	//sPL pl;
+	//pl.center = vec4(normalize(vec3(5,5,5)) * 2.0,1);
+	//pl.color = vec4(1,1,1,1);
+	//pl.intensity = 100.0;
+	
+	float diffuseIntensity = fragDiffuseIntensity();//vVertexIntensity;
+	
+	float specIntensity = calcSpecIntensity(specular);
 	
 	vec4 color = texture(uTex, vTexCoord + offset);//vec4(normal,1);
 	
 	vec4 globalAmbientColor = color;
-	float globalAmbientIntensity = 0.15;
+	float globalAmbientIntensity = 0.05;
 	
 	vec4 finalColor = 
         globalAmbientColor * globalAmbientIntensity +
         (
             (diffuseIntensity * color) + 
-            (specIntensity) 
-        ) * pl.color;
+            (specIntensity * specularMask) 
+        ) * pl.color + 
+        max(0.0,0.5 - diffuseIntensity * 2.0) * lightMask * 2.0;
     
     
 	rtFragColor = finalColor;//texture(uTex, vTexCoord);
--- shaders/Project_ShaderVS.glsl
+++ shaders/Project_ShaderVS.glsl
@@ -10,15 +10,45 @@ uniform mat4 uModelMat, uViewMat, uProjMat;
 uniform float uTime;
 
 out vec2 vTexCoord;
-out vec3 normal;
+out vec3 vNormal;
 out vec4 position;
 out float time;
+out float vVertexDiffuse;
+
+struct sPL
+{
+	vec4 center;
+	vec4 color;
+	float intensity;
+};
+
+out sPL pl;
+
+void calcDiffuseIntensity(sPL pl)
+{
+	vec3 lightVector = normalize(pl.center.xyz);
+	float diffuseCoefficient = max(0.0,dot(aNormal,lightVector));
+	
+	float slDist = distance(pl.center.xyz,aPosition.xyz);
+	
+	//Distance over intensity
+    float dOi = (slDist/pl.intensity);
+    
+    float attenuatedIntensity = 1.0/(1.0 + dOi + (dOi * dOi));
+    
+    vVertexDiffuse = diffuseCoefficient * attenuatedIntensity;
+}
 
 void main() {
 	mat4 modelViewProjMat = uProjMat * uViewMat * uModelMat;
 	gl_Position = modelViewProjMat * aPosition;
 	vTexCoord = aTexCoord;
-	normal = aNormal;
+	vNormal = aNormal;
 	position = aPosition;
 	time = uTime;
+	
+	pl.center = vec4(5,5,5,1);
+	pl.color = vec4(1,1,1,1);
+	pl.intensity = 100.0;
+	calcDiffuseIntensity(pl);
 }
